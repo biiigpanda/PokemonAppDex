93a17b98aa945954f71af9e9815644c87ad0ef67
Marc Gallardo <marc.gallardus@gmail.com>
2025-01-12 19:34:00 +0100

    init commit

---
+++ PokemonAppDex/PokemonAppDex/PokemonAppDexApp.swift
@@ -0,0 +1,17 @@
+//
+//  PokemonAppDexApp.swift
+//  PokemonAppDex
+//
+//  Created by Marc Gallardo on 12/1/25.
+//
+
+import SwiftUI
+
+@main
+struct PokemonAppDexApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
