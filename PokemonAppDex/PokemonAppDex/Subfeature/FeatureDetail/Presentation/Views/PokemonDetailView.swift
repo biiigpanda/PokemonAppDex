b4a7c9708be9e8ea3ac251c23ae52a48359e2467
Marc Gallardo <marc.gallardus@gmail.com>
2025-02-21 01:14:06 +0100

    Barras agregadas al detalle de pokemon

--- PokemonAppDex/PokemonAppDex/Subfeature/FeatureDetail/Presentation/Views/PokemonDetailView.swift
+++ PokemonAppDex/PokemonAppDex/Subfeature/FeatureDetail/Presentation/Views/PokemonDetailView.swift
@@ -10,12 +10,12 @@ import SwiftUI
 
 struct PokemonDetailView: View {
     @StateObject private var viewModel: PokemonDetailViewModel
-    @State private var barWidth: CGFloat = 0
+    @State var barWidth: Double = 0.0
     
     init(_ viewModel: PokemonDetailViewModel) {
         _viewModel = StateObject(wrappedValue: viewModel)
     }
-
+    
     var body: some View {
         VStack {
             if viewModel.pokemonDetail == nil {
@@ -26,48 +26,58 @@ struct PokemonDetailView: View {
                                 id: viewModel.pokemonDetail?.pokemon.id ?? 0)
                 .frame(maxWidth: 400, maxHeight: 400)
                 .padding(.horizontal, 12)
-
+                
             }
-            VStack {
-                // for each por cada valor en el stat
-                HStack(spacing: 8, content: {
-//                    ForEach(viewModel.getPokemonStats(), id: \.self) { pokemonStat in
-                    Text(viewModel.pokemonDetail?.stats.first?.stat.name ?? "")
-                        Text("\(viewModel.pokemonDetail?.stats.first?.baseStat ?? 0)")
-                        atackBar
-                            .padding(.horizontal, 13)
-                            .padding(.top, 8)
-                            .onAppear {
-                                withAnimation(.easeOut(duration: 1.0)) {
-                                    barWidth = 0.8
-                                }
-                            }
-//                    }
-                })
-                .padding(.horizontal, 4)
+            VStack(spacing: 4) {
+                ForEach(viewModel.getPokemonStats(), id: \.stat.name) { pokemonStat in
+                    HStack() {
+                        Text(pokemonStat.stat.name)
+                            .frame(maxWidth: UIScreen.main.bounds.width * 0.25)
+                            .scenePadding()
+                        Text("\(pokemonStat.baseStat)")
+                            .frame(maxWidth: UIScreen.main.bounds.width * 0.25)
+                        BarView(value: Double(pokemonStat.baseStat) / 255.0)
+                            .frame(maxWidth: UIScreen.main.bounds.width * 0.5)
+                    }
+                }
             }
+            .background(Color(red: 100.0/255.0, green: 205.0/255.0, blue: 189.0/255.0, opacity: 0.5))
+            .frame(maxWidth: 400, maxHeight: 400)
             .padding(.horizontal, 12)
+            .padding(.bottom, 12)
         }
         .onAppear {
             viewModel.onAppear()
         }
     }
     
-    var atackBar: some View {
-        Color.clear
-            .frame(maxWidth: .infinity, maxHeight: 25)
-            .overlay(GeometryReader { gp in
-                HStack(spacing: 0) {
-                    Rectangle()
-                        .fill(Color.orange)
-                        .frame(width: barWidth * gp.size.width)
-                        .border(.black)
-                    Rectangle()
-                        .fill(Color.clear)
-                        .frame(width: (1 - barWidth) * gp.size.width)
-                        .border(.black)
+    // he puesto un elemento independiente las barras para que se actualice correctamente la anchura de ellas
+    
+    struct BarView: View {
+        let value: Double
+        @State private var animatedWidth: Double = 0.0
+        
+        var body: some View {
+            Color.clear
+                .frame(maxWidth: .infinity, maxHeight: 25)
+                .overlay(GeometryReader { gp in
+                    HStack(spacing: 0) {
+                        Rectangle()
+                            .fill(Color.orange)
+                            .frame(width: animatedWidth * gp.size.width)
+                            .border(.black)
+                        Rectangle()
+                            .fill(Color.clear)
+                            .frame(width: (1 - animatedWidth) * gp.size.width)
+                            .border(.black)
+                    }
+                })
+                .clipShape(RoundedRectangle(cornerRadius: 6))
+                .onAppear {
+                    withAnimation(.easeOut(duration: 1.0)) {
+                        animatedWidth = value
+                    }
                 }
-            })
-            .clipShape(RoundedRectangle(cornerRadius: 6))
+        }
     }
 }
