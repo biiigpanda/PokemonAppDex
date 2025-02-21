721df3601e1d07d7794d65f82d584a4f029e2e91
Marc Gallardo <marc.gallardus@gmail.com>
2025-02-21 11:34:05 +0100

    add new bars for stats of pokemon

--- PokemonAppDex/PokemonAppDex/Subfeature/FeatureDetail/Presentation/Views/PokemonDetailView.swift
+++ PokemonAppDex/PokemonAppDex/Subfeature/FeatureDetail/Presentation/Views/PokemonDetailView.swift
@@ -12,6 +12,9 @@ struct PokemonDetailView: View {
     @StateObject private var viewModel: PokemonDetailViewModel
     @State var barWidth: Double = 0.0
     
+    let arrayColors: [Color] = [.green, .red, .blue, .purple, .pink, .yellow]
+
+    
     init(_ viewModel: PokemonDetailViewModel) {
         _viewModel = StateObject(wrappedValue: viewModel)
     }
@@ -24,27 +27,31 @@ struct PokemonDetailView: View {
                 PokemonCellView(name: viewModel.pokemonDetail?.pokemon.name ?? "",
                                 imageURL: viewModel.pokemonDetail?.pokemon.imageURL ?? URL(string: ""),
                                 id: viewModel.pokemonDetail?.pokemon.id ?? 0)
-                .frame(maxWidth: 400, maxHeight: 400)
+                .frame(maxWidth: 320, maxHeight: 350)
                 .padding(.horizontal, 12)
                 
             }
-            VStack(spacing: 4) {
-                ForEach(viewModel.getPokemonStats(), id: \.stat.name) { pokemonStat in
+            VStack {
+                Text("Stats")
+                    .font(.custom("Gameplay", size: 16))
+                    .padding(.top, 8)
+                ForEach(Array(viewModel.getPokemonStats().enumerated()), id: \.element.stat.name) { index, pokemonStat in
                     HStack() {
                         Text(pokemonStat.stat.name)
                             .frame(maxWidth: UIScreen.main.bounds.width * 0.25)
                             .scenePadding()
                         Text("\(pokemonStat.baseStat)")
                             .frame(maxWidth: UIScreen.main.bounds.width * 0.25)
-                        BarView(value: Double(pokemonStat.baseStat) / 255.0)
-                            .frame(maxWidth: UIScreen.main.bounds.width * 0.5)
+                        BarView(value: Double(pokemonStat.baseStat) / 255.0,
+                                barColor: arrayColors[index % arrayColors.count])
+                        .frame(maxWidth: UIScreen.main.bounds.width * 0.5)
                     }
+                    .padding(.horizontal, 8)
                 }
             }
-            .background(Color(red: 100.0/255.0, green: 205.0/255.0, blue: 189.0/255.0, opacity: 0.5))
-            .frame(maxWidth: 400, maxHeight: 400)
-            .padding(.horizontal, 12)
-            .padding(.bottom, 12)
+            .background(Color(red: 208.0/255.0, green: 205.0/255.0, blue: 189.0/255.0, opacity: 0.5))
+            .clipShape(RoundedRectangle(cornerRadius: 8.0))
+            .frame(maxWidth: 320, maxHeight: .infinity)
         }
         .onAppear {
             viewModel.onAppear()
@@ -55,15 +62,16 @@ struct PokemonDetailView: View {
     
     struct BarView: View {
         let value: Double
+        let barColor: Color
         @State private var animatedWidth: Double = 0.0
         
         var body: some View {
             Color.clear
-                .frame(maxWidth: .infinity, maxHeight: 25)
+                .frame(maxWidth: .infinity, maxHeight: 20)
                 .overlay(GeometryReader { gp in
                     HStack(spacing: 0) {
                         Rectangle()
-                            .fill(Color.orange)
+                            .fill(barColor)
                             .frame(width: animatedWidth * gp.size.width)
                             .border(.black)
                         Rectangle()
@@ -72,6 +80,7 @@ struct PokemonDetailView: View {
                             .border(.black)
                     }
                 })
+
                 .clipShape(RoundedRectangle(cornerRadius: 6))
                 .onAppear {
                     withAnimation(.easeOut(duration: 1.0)) {
@@ -80,4 +89,8 @@ struct PokemonDetailView: View {
                 }
         }
     }
+}
+
+#Preview {
+    PokemonDetailView(PokemonDetailViewModel(dto: PokemonDetailAssemblyDTO(idPokemon: 1, urlImage: URL(string: "https://raw.githubusercontent.com/PokeAPI/sprites/master/sprites/pokemon/1.png" ?? "")!)))
 }
